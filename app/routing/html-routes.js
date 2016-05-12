8c4a1fc43fe4645fa355d1d2ca583d995036ef39
leslieo23 <leslie.obour23@gmail.com>
2016-05-11 21:38:23 -0400

    form validation

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -7,6 +7,9 @@ module.exports=function(app){
 	app.get('/deeds',function(req,res){
 		res.sendFile(path.join(__dirname+'/../public/deeds.html'))
 	});
+	app.get('/profile',function(req,res){
+		res.sendFile(path.join(__dirname+ '/../public/profile.html'))
+	})
 	app.use(function(req,res){
 		res.sendFile(path.join(__dirname+'/../public/index.html'))
 	});
